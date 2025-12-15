7e0450968640f7c6ff1b69f275f2451fa8edd924
MiguelZJL333 <miguelzambrano20333@gmail.com>
2025-12-14 23:15:50 -0300

    Partre de la entrega

--- src/app.js
+++ src/app.js
@@ -1,18 +1,60 @@
+//Importaciones
 import express from 'express';
 import productsRouter from './ProductManager.js';
 import CartManager from './CartManager.js';
+import http from 'http';
+import { engine } from "express-handlebars";
+import viewsRouter from './routes/views.router.js';
+import { Server } from 'socket.io';
 
+//Inicializaciones
 const app = express();
 const cartManager = new CartManager();
+const server = http.createServer(app);
+
 app.use(express.json());
+
+//WebSocket
+const io = new Server(server);
+
+//websocket desde el servidor
+io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    //emitimos un mensaje al cliente
+    socket.emit("message history", messages);
+
+    // Escuchar el evento "Nuevo Mensaje" desde el cliente
+    socket.on("Nuevo Mensaje", (data) => {
+        messages.push(data); 
+        io.emit("broadcast new message", data); // Emitir el evento "Actualizar Mensajes" a todos los clientes conectados
+    });
+})
+
+//Configuraciones
+app.use(express.static('public')); 
+
+// Handlebars configuration
+app.engine('handlebars', engine()); 
+app.set('view engine', 'handlebars'); 
+app.set('views', './src/views');
+
+//Endpoints de vistas
+app.use('/', viewsRouter);
+
+//percitencia en memoria
+const messages = [];    
+
 
 // Montar router de productos
 app.use('/api/products', productsRouter);
 
 // Server
 app.listen(8080, () => {
-    console.log('Server is running on port 8080');
+    console.log('Corriendo en el puerto 8080, http://localhost:8080');
 });
+
+//--------------------------------Carts--------------------------------------------
 
 // Alias: aceptar también la ruta plural '/products/:id' para compatibilidad
 app.post('/api/carts/:cid/products/:id', async (req, res) => {
